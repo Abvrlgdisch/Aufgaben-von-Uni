42883235d6fb64c2bcd4761fedd6618da1ea93f3
Abvrlgdisch <72340425+Abvrlgdisch@users.noreply.github.com>
2020-10-05 18:36:02 +0300

    Add files via upload

---
+++ Aufgabe 3/Aufgabe 3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Aufgabe_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравствуйте!");
+            Console.WriteLine("Предлагаю Вам посчитать площадь и периметр прямоугольника по двум сторонам.");
+            Console.WriteLine("Для этого, предлагаю Вам ввести две стороны вышеуказанной фигуры");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Площадь прямоугольника будет равна: "+ (a*b));
+            Console.WriteLine("Периметр прямоугольника будет равен " + (2 * (a + b)));
+        }
+    }
+}
